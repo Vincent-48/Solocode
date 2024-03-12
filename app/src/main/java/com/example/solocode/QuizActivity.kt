50941dea93c1941ad1d492f58d47a045a7aa3887
Vince <vincentochieng33@gmail.com>
2024-03-12 22:23:23 +0300

    initial commit

--- app/src/main/java/com/example/solocode/QuizActivity.kt
+++ app/src/main/java/com/example/solocode/QuizActivity.kt
@@ -3,10 +3,12 @@ package com.example.solocode
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.view.View
+import android.widget.Button
 import com.example.solocode.databinding.ActivityMainBinding
 import com.example.solocode.databinding.ActivityQuizBinding
 
-class QuizActivity : AppCompatActivity() {
+class QuizActivity : AppCompatActivity(), View.OnClickListener {
     companion object{
         var questionModelList: List<QuestionModel> = listOf()
         var time : String = ""
@@ -14,11 +16,22 @@ class QuizActivity : AppCompatActivity() {
     lateinit var binding: ActivityQuizBinding
 
     var currentQuestionIndex = 0;
+    var selectedAnswer = ""
+    var score = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityQuizBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.apply {
+            btn0.setOnClickListener(this@QuizActivity)
+            btn1.setOnClickListener(this@QuizActivity)
+            btn2.setOnClickListener(this@QuizActivity)
+            btn3.setOnClickListener(this@QuizActivity)
+            nextBtn.setOnClickListener(this@QuizActivity)
+
+        }
+
         loadQuestions()
         startTimer()
     }
@@ -28,7 +41,7 @@ class QuizActivity : AppCompatActivity() {
             override fun onTick(millisUntilFinished: Long) {
                 val seconds = millisUntilFinished/1000
                 val minutes = seconds/60
-                val remainingSeconds = minutes % 60
+                val remainingSeconds = seconds % 60
                 binding.timerIndicatorTextview.text = String.format("%02d:%02d", minutes,remainingSeconds)
             }
 
@@ -36,7 +49,7 @@ class QuizActivity : AppCompatActivity() {
                 //Finish the quiz
             }
 
-        }
+        }.start()
     }
 
     private fun loadQuestions(){
@@ -49,6 +62,26 @@ class QuizActivity : AppCompatActivity() {
             btn1.text = questionModelList[currentQuestionIndex].options[1]
             btn2.text = questionModelList[currentQuestionIndex].options[2]
             btn3.text = questionModelList[currentQuestionIndex].options[3]
+        }
+    }
+
+    override fun onClick(v: View?) {
+
+        binding.apply {
+            btn0.setBackgroundColor(getColor(R.color.gray))
+            btn1.setBackgroundColor(getColor(R.color.gray))
+            btn2.setBackgroundColor(getColor(R.color.gray))
+            btn3.setBackgroundColor(getColor(R.color.gray))
+        }
+        val clickedBtn = v as Button
+        if (clickedBtn.id == R.id.next_btn){
+            //nextbtn is clicked
+            currentQuestionIndex++
+            loadQuestions()
+        }else{
+            //options button is clicked
+            clickedBtn.setBackgroundColor(getColor(R.color.orange))
+            selectedAnswer = clickedBtn.text.toString()
         }
     }
 }
