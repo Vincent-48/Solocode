8661d195541149a1acb421bb186f6373fea70b61
Vince <vincentochieng33@gmail.com>
2024-03-12 20:48:16 +0300

    initial commit

--- app/src/main/java/com/example/solocode/QuizActivity.kt
+++ app/src/main/java/com/example/solocode/QuizActivity.kt
@@ -2,25 +2,53 @@ package com.example.solocode
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import com.example.solocode.databinding.ActivityMainBinding
+import com.example.solocode.databinding.ActivityQuizBinding
 
 class QuizActivity : AppCompatActivity() {
     companion object{
         var questionModelList: List<QuestionModel> = listOf()
+        var time : String = ""
     }
-    lateinit var binding: ActivityMainBinding
+    lateinit var binding: ActivityQuizBinding
 
-    var currentQuestionIndex = 0
+    var currentQuestionIndex = 0;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityQuizBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        loadQuestions()
+        startTimer()
+    }
+    private fun startTimer(){
+        val totalTimeInMillis = time.toInt() * 60 * 1000L
+        object : CountDownTimer(totalTimeInMillis,1000L){
+            override fun onTick(millisUntilFinished: Long) {
+                val seconds = millisUntilFinished/1000
+                val minutes = seconds/60
+                val remainingSeconds = minutes % 60
+                binding.timerIndicatorTextview.text = String.format("%02d:%02d", minutes,remainingSeconds)
+            }
+
+            override fun onFinish() {
+                //Finish the quiz
+            }
+
+        }
     }
 
     private fun loadQuestions(){
         binding.apply {
-
+            questionIndicatorTextview.text = "Question ${currentQuestionIndex+1}/ ${questionModelList.size}"
+            questionProgressIndicator.progress =
+                (currentQuestionIndex.toFloat() / questionModelList.size.toFloat() * 100).toInt()
+            questionTextview.text = questionModelList[currentQuestionIndex].question
+            btn0.text = questionModelList[currentQuestionIndex].options[0]
+            btn1.text = questionModelList[currentQuestionIndex].options[1]
+            btn2.text = questionModelList[currentQuestionIndex].options[2]
+            btn3.text = questionModelList[currentQuestionIndex].options[3]
         }
     }
 }
